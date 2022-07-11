815f4513e5e9a8545ec35c002ba7a3e063ab1d38
windiest.xie <windiest.xie@cloudwise.com>
2022-07-11 15:56:09 +0800

    DBCP2、1

--- src/main/java/com/exercise/DBCP/util/DBCPUtil.java
+++ src/main/java/com/exercise/DBCP/util/DBCPUtil.java
@@ -3,7 +3,6 @@ package com.exercise.DBCP.util;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
