706613f1dcf02df6f042541aa7f24b0ce0dcc56f
windiest.xie <windiest.xie@cloudwise.com>
2022-07-30 18:03:44 +0800

    aoto save

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,11 +4,19 @@ package com.example;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!".concat("windi"));
-        //  ==========================================================================================================================================================================================
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!".concat("windi"));
+    System.out.println("Hello World!".concat("windi"));
+    System.out.println("Hello World1!".concat("windi1"));
+    int xieXieNi;
+    xieXieNi = 3;
+
+    System.out.println(xieXieNi);
+    for (int i = 0; i < args.length; i++) {
+      for (int j = 0; j < args.length; j++) {
+        
+      }
     }
+  }
 }
