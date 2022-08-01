cf096830df2ec1cd4af7345713193ce9c65faac3
windiest.xie <windiest.xie@cloudwise.com>
2022-08-01 15:02:14 +0800

    优化结构1

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,28 +5,28 @@ package com.example;
  *
  */
 public class App {
-  public static void main(String[] args) {
-    System.out.println("Hello World!".concat("windy"));
-    System.out.println("Hello World!".concat("windy"));
-    sayHello();
-    int xieXieNi;
-    xieXieNi = 3;
+    public static void main(String[] args) {
+        System.out.println("Hello World!".concat("windy"));
+        System.out.println("Hello World!".concat("windy"));
+        sayHello();
+        int xieXieNi;
+        xieXieNi = 3;
 
-    System.out.println(xieXieNi);
-    for (int i = 0; i < args.length; i++) {
-      for (int j = 0; j < args.length; j++) {
-        for (int j2 = 0; j2 < args.length; j2++) {
-          for (int k = 0; k < args.length; k++) {
-            for (int k2 = 0; k2 < args.length; k2++) {
-              
+        System.out.println(xieXieNi);
+        for (int i = 0; i < args.length; i++) {
+            for (int j = 0; j < args.length; j++) {
+                for (int j2 = 0; j2 < args.length; j2++) {
+                    for (int k = 0; k < args.length; k++) {
+                        for (int k2 = 0; k2 < args.length; k2++) {
+
+                        }
+                    }
+                }
             }
-          }
         }
-      }
     }
-  }
 
-  private static void sayHello() {
-    System.out.println("Hello World1!".concat("windy1"));
-  }
+    private static void sayHello() {
+        System.out.println("Hello World1!".concat("windy1"));
+    }
 }
