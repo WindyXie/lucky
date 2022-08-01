d7a0ed80b7aa5b28255837be37c76ccedb4dddd4
windiest.xie <windiest.xie@cloudwise.com>
2022-08-01 21:44:17 +0800

    启动起来了，但是没法访问？

--- src/main/java/com/exercise/useSpringBoot/controller/ShowController.java
+++ src/main/java/com/exercise/useSpringBoot/controller/ShowController.java
@@ -5,17 +5,19 @@ import java.util.List;
 import javax.annotation.Resource;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.exercise.mybatis.dao.Dir;
 import com.exercise.mybatis.dao.dirMapper;
 
-@RestController("")
+@RestController
+@RequestMapping(value = "/xx")
 public class ShowController {
     @Resource
     dirMapper dirMapper;
 
-    @GetMapping("")
+    @GetMapping("/get")
     public List<Dir> get() {
         return dirMapper.sel();
     }
