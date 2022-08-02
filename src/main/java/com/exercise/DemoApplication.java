91aff21f94095f8dc3c0c925a7d3ff442573a168
windiest.xie <windiest.xie@cloudwise.com>
2022-08-02 13:46:29 +0800

    注意componentScan扫描范围

--- src/main/java/com/exercise/DemoApplication.java
+++ src/main/java/com/exercise/DemoApplication.java
@@ -2,10 +2,10 @@ package com.exercise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+// import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(value = "com.exercise")
+// @ComponentScan(value = "com.exercise")
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
