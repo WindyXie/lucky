08d7b57d5c4f2be7889d3c26a7237a9b96bdf9b7
windiest.xie <windiest.xie@cloudwise.com>
2022-08-02 13:42:17 +0800

    注意application的位置

--- src/main/java/com/exercise/DemoApplication.java
+++ src/main/java/com/exercise/DemoApplication.java
@@ -1,11 +1,11 @@
-package com.exercise.useSpringBoot;
+package com.exercise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.exercise.mybatis.dao")
+@ComponentScan(value = "com.exercise")
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
--- src/main/java/com/exercise/useSpringBoot/controller/ShowController.java
+++ src/main/java/com/exercise/useSpringBoot/controller/ShowController.java
@@ -2,8 +2,8 @@ package com.exercise.useSpringBoot.controller;
 
 import java.util.List;
 
-import javax.annotation.Resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +14,16 @@ import com.exercise.mybatis.dao.dirMapper;
 @RestController
 @RequestMapping(value = "/xx")
 public class ShowController {
-    @Resource
+    @Autowired
     dirMapper dirMapper;
 
     @GetMapping("/get")
     public List<Dir> get() {
         return dirMapper.sel();
+    }
+
+    @GetMapping("/get1")
+    public String get1() {
+        return "1";
     }
 }
