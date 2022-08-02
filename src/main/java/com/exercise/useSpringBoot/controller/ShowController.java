141a87bb6ed312d79e49d3990a9a7de518540a01
windiest.xie <windiest.xie@cloudwise.com>
2022-08-02 14:08:09 +0800

    相同的写法

--- src/main/java/com/exercise/useSpringBoot/controller/ShowController.java
+++ src/main/java/com/exercise/useSpringBoot/controller/ShowController.java
@@ -6,18 +6,20 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.exercise.mybatis.dao.Dir;
 import com.exercise.mybatis.dao.dirMapper;
 
-@RestController
-@RequestMapping(value = "/xx")
+@RestController //相当于@Controller + @ResponseBody
+// @RequestMapping(value = "/xx")
 public class ShowController {
     @Autowired
     dirMapper dirMapper;
 
-    @GetMapping("/get")
+    // @GetMapping("/get")
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
     public List<Dir> get() {
         return dirMapper.sel();
     }
